c5a93d811305abb66dfb4c0ad7c951ee0755e907
kelvin <kelvin86@gmail.com>
2017-09-11 10:16:30 +0800

    Completed HMM recognizer

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -31,6 +31,8 @@ class ModelSelector(object):
     def select(self):
         raise NotImplementedError
 
+
+
     def base_model(self, num_states):
         # with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -77,7 +79,36 @@ class SelectorBIC(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+
+        # best_num_state = None
+        alpha = 1.5
+        best_bic_score = float('+inf')
+        best_model = None
+        for num_state in range(self.min_n_components,self.max_n_components+1):
+
+            try:
+                hmm_model = GaussianHMM(n_components=num_state, covariance_type="diag", n_iter=1000,
+                    random_state=self.random_state, verbose=False).fit(self.X,self.lengths)
+
+                logL = hmm_model.score(self.X,self.lengths)
+                n_parameters = num_state * num_state + 2 * num_state * len(self.X[0]) -1
+                bic_score = -2 * logL + alpha * n_parameters * np.log(len(self.X))
+
+                # print("Training {} with {} states. BIC score: {}".format(self.this_word, num_state, bic_score))
+                if bic_score < best_bic_score:
+                    best_bic_score = bic_score
+                    best_model = hmm_model
+                    # print("BestModel for {} set to {} states".format(self.this_word,num_state))
+
+            except:
+                pass
+
+
+        
+
+
+
+        return best_model if best_model is not None else self.base_model(self.n_constant)
 
 
 class SelectorDIC(ModelSelector):
@@ -89,20 +120,209 @@ class SelectorDIC(ModelSelector):
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
+    def logL_anti_likelihood_terms(self, model):
+        # print("LogL anti likelihood terms")
+        # print("all words:{}".format(self.words))
+
+        # antilogLikelihoodArr=[]
+
+        antiLL = 0.0
+        words_counter = 0
+        for word, features in self.hwords.items():
+            if word != self.this_word:
+                X, lengths = features
+                # print("Current word detected:{} value: {}".format(word,features))
+                # print("antilog X len:{} , len lengths : {} for {}".format(len(X), len(lengths), word))
+
+                #Train model
+                try:
+                    # model = GaussianHMM(n_components = num_state, covariance_type = "diag", n_iter=1000,
+                    #     random_state= self.random_state, verbose=False).fit(X, lengths)
+
+                    logL = model.score(X,lengths)
+
+
+                    # print("Antiloglikelihood logL:{}".format(logL))
+
+                    # if logL is not None:
+                    antiLL += logL
+                    words_counter+=1
+                        # antilogLikelihoodArr.append(logL)
+
+                except Exception as e: 
+                    print("Error: {}".format(e))
+                    pass
+
+
+        #get Average of logL
+        # print("antiloglikelihoodArr:{}".format(antilogLikelihoodArr))
+        # avgAntiLogLikelihood = np.mean(antilogLikelihoodArr)
+        # print("AvgAntiLogLikelihood: {}".format(avgAntiLogLikelihood))
+
+
+        return antiLL / words_counter
+
+
+
+
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+
+        best_dic_score = float('-inf')
+        best_model = None
+
+        # print("len Self.X :{}\n len Self.lengths:{} for {}".
+        #     format(len(self.X),len(self.lengths),self.this_word))
+        
+
+
+        #self.min_n_components, self.max_n_components +1 
+        for num_state in range(self.min_n_components, self.max_n_components +1):
+            try:
+                hmm_model = GaussianHMM(n_components = num_state, covariance_type = "diag", n_iter=1000,
+                    random_state= self.random_state, verbose=False).fit(self.X, self.lengths)
+
+                logL = hmm_model.score(self.X, self.lengths)
+                # print("LogL score for {} : {} ".format(self.this_word, logL))
+                antiLogL = self.logL_anti_likelihood_terms(hmm_model)
+                dic_score = logL - antiLogL
+                # print("logL:{}, avg antiLogL :{} , score:{} for {} for {} components".
+                #     format(logL,antiLogL,dic_score, self.this_word,num_state))
+
+
+                # if best_dic_score == None:
+                #     best_dic_score = dic_score
+                #     best_model = hmm_model
+
+                if dic_score > best_dic_score:
+                    best_dic_score = dic_score
+                    best_model = hmm_model
+
+            except Exception as e: 
+                # print("Error:{}".format(e))
+                pass
+
         # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        return best_model if best_model is not None else self.base_model(self.n_constant)
+
+    def test(self):
+        # print("SelectorDIC Test:")
+        # print("self words:{}, length:{}".format(self.words, len(self.words)))
+
+        # print("self hwords:{}, length:{}".format(self.hwords, len(self.hwords)))        
+        print("This word:{}".format(self.this_word))
+        val = self.hwords[self.this_word]
+        print("This word val:{}, length of val[0] : {}, length of val [1] : {}".
+            format(val,len(val[0]), len(val[1])))
+
+        for key, val in self.hwords.items():
+            if key == self.this_word:
+                X, lengths = val
+                print("This word val:{}, length of val[0] : {}, length of val [1] : {}".
+                    format(val,len(val[0]), len(val[1])))
+                print("array equals: X:{}, lengths:{}\nlen(X):{}, len(lengths):{}"
+                    .format(X,lengths,len(X),len(lengths)))
+
+
 
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
 
     '''
+    
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
+        
+        default_split = min(len(self.sequences),3)
+ 
+        
+        
+        
+        bestModel = None
+        bestLogL = float('-inf')
+        
+        best_num_state = None
+        avg_log_likelihood = {}
         # TODO implement model selection using CV
-        raise NotImplementedError
+        for num_state in range(self.min_n_components,self.max_n_components+1):
+            #hm_model = self.base_model(x)
+            avg_logL = None
+            scores = []
+            
+            if default_split >= 2:    
+                split_method = KFold(n_splits = default_split)
+                
+
+                
+                # hmm_model = None
+                # logL = None 
+                try:
+                    
+                    for cv_train_idx , cv_test_idx in split_method.split(self.sequences):
+                        cv_train_X , cv_train_lengths = combine_sequences(cv_train_idx, self.sequences)
+                        testX , testLengths = combine_sequences(cv_test_idx, self.sequences)
+                        
+                        hmm_model = GaussianHMM(n_components=num_state, covariance_type="diag", n_iter=1000,
+                                        random_state=self.random_state, verbose=False).fit(cv_train_X,cv_train_lengths)
+
+
+
+                        logL = hmm_model.score(testX, testLengths)
+
+                        scores.append(logL)
+                    avg_logL = np.mean(scores)
+
+                except:
+                    pass
+            else:
+                #default split < 2:
+                try:
+                    hmm_model = GaussianHMM(n_components=num_state, covariance_type="diag", n_iter=1000,
+                                            random_state=self.random_state, verbose=False).fit(self.X,self.lengths)
+
+
+
+                    logL = hmm_model.score(self.X,self.lengths)
+
+                    scores.append(logL)
+                    avg_logL = no.mean(scores)
+                except:
+                    pass
+
+
+            #get average logL for current num_state
+            # print("{} Average log likelihood :{} for num_state:{}".format(self.this_word,avg_logL,num_state))
+
+            if not best_num_state and avg_logL is not None:
+                bestLogL = avg_logL
+                best_num_state = num_state
+            if avg_logL is not None and avg_logL > bestLogL:
+                bestLogL = avg_logL
+                best_num_state = num_state
+                # print("Bestmodel for {} now has {} states with logL of {}".format(self.this_word,num_state,avg_logL))
+        
+        if best_num_state:
+            bestModel = GaussianHMM(n_components=best_num_state, covariance_type="diag", n_iter=1000,
+                                    random_state=self.random_state, verbose=False).fit(self.X,self.lengths)
+
+    #                 print("Score: {}".format(score))
+    #                 print("Train_Seq: {}".format(train_seq))
+                
+                
+                #test_seq = [ self.sequences[j] for j in cv_test_idx ][0]
+
+                
+                
+                
+#                 print("Test_seq: {}".format(test_seq))
+                
+                
+#                 hmm_model, logL = self.base(num_states, train_seq, test_seq)
+#                 if not hmm_model:
+#                     continue
+        
+        #print("BestModel return with state: {}".format())
+        return bestModel if bestModel is not None else self.base_model(self.n_constant)
--- my_recognizer.py
+++ my_recognizer.py
@@ -17,9 +17,57 @@ def recognize(models: dict, test_set: SinglesData):
        guesses is a list of the best guess words ordered by the test set word_id
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
+
+
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-    probabilities = []
+    # probabilities = list(dict(None))* len(test_set.get_all_sequences())
+
+    probabilities = [ {} for x in range(len(test_set.get_all_sequences()))]
+    # print("Probabilities: {}, len:{}".format(probabilities,len(probabilities)))
     guesses = []
     # TODO implement the recognizer
     # return probabilities, guesses
-    raise NotImplementedError
+
+    print("in recognize")
+    # print("Models dic:\n{}\n len(models):{}".format(models,len(models)))
+
+    # print("test_set singlesData:{}\nget_all_sequences :{}\n len(get_all_sequences):{}".format(test_set,test_set.get_all_sequences(),len(test_set.get_all_sequences())))
+    # print("test_set get_all_Xlengths:{} len:{} \n".format( test_set.get_all_Xlengths(),len(test_set.get_all_Xlengths())))
+    # print("test_set get_all_Xlengths()[177]:\n{}".format(test_set.get_all_Xlengths()[177]))    
+
+
+    for key, value in test_set.get_all_sequences().items():
+
+      # print("key:{}, value:{}".format(key,value))
+      # print("get_all_Xlengths()[{}]: {}".format(key,test_set.get_all_Xlengths()[key]))
+      X, lenghts = test_set.get_all_Xlengths()[key] 
+      prob = {}
+
+      best_logL = float('-inf')
+      best_word = None
+      for word,model in models.items():
+        try:
+
+          score = model.score(X,lenghts) 
+          # word_score = {}
+          # word_score[word] = score
+          probabilities[key][word] =  score
+
+          if best_word == None:
+            best_logL = score
+            best_word = word
+
+          if score > best_logL:
+            best_logL = score
+            best_word = word
+        except Exception as e:
+          pass
+
+      #update guesses with best score
+      guesses.append(best_word)
+
+
+    # print("Probabilities final : \n{},\nlen:{}".format(probabilities,len(probabilities)))
+    # print("Guesses final:\n{},\nlen:{}".format(guesses,len(guesses)))
+
+    return probabilities, guesses
